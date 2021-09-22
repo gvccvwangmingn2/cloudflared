cc2a9e97908133e1c93a68b8aac0cf7d1af024aa
gvccvwangmingn2 <74752766+gvccvwangmingn2@users.noreply.github.com>
2021-09-23 02:17:02 +0700

    Update http2.go

--- connection/http2.go
+++ connection/http2.go
@@ -58,9 +58,7 @@ func NewHTTP2Connection(
 ) *HTTP2Connection {
 	return &HTTP2Connection{
 		conn: conn,
-		server: &http2.Server{
-			MaxConcurrentStreams: math.MaxUint32,
-		},
+		server: &http2.Server{},
 		config:               config,
 		connOptions:          connOptions,
 		observer:             observer,
@@ -227,13 +225,6 @@ func (rp *http2RespWriter) Read(p []byte) (n int, err error) {
 }
 
 func (rp *http2RespWriter) Write(p []byte) (n int, err error) {
-	defer func() {
-		// Implementer of OriginClient should make sure it doesn't write to the connection after Proxy returns
-		// Register a recover routine just in case.
-		if r := recover(); r != nil {
-			println("Recover from http2 response writer panic, error", r)
-		}
-	}()
 	n, err = rp.w.Write(p)
 	if err == nil && rp.shouldFlush {
 		rp.flusher.Flush()
